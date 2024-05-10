516d7ba53158a1c67f8000f393fc54594662c6c0
santosh rai <santoshrai2072001@gmail.com>
2024-05-10 17:33:03 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning project to predict the trip duration for taxi in NYC',
+    author='Santosh rai',
+    license='MIT',
+)
